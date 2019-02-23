63499baafae32e76c3d81b9c7123b6a9979d3083
myflowpl <pedro.blaszczak@gmail.com>
2019-02-23 11:25:47 +0100

    create

--- src/app/rxjs/basic/create.component.ts
+++ src/app/rxjs/basic/create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ListComponent } from 'src/app/shared/list/list.component';
 import { Observable, fromEvent, interval, of, empty, EMPTY, range, timer, throwError } from 'rxjs';
+import { share } from 'rxjs/operators';
 
 @Component({
   selector: 'app-create',
@@ -28,6 +29,81 @@ export class CreateComponent implements OnInit {
     const log = (...args) => this.list.add(...args);
     const button = this.btn.nativeElement;
 
+    // const stream$ = interval(500).pipe(share());
+    // const stream$ = of({id: 2, name: 'Piotr'});
+    // const stream$ = empty();
+    // const stream$ = EMPTY;
+    // const stream$ = range(2, 4);
+    // const stream$ = timer(2000);
+    const stream$ = throwError('custom error');
+
+
+    const sub = stream$.subscribe(
+      (val) => log('A', val),
+      (err) => log('error', err),
+      () => log('complete')
+    );
+
+    // const sub2 = stream$.subscribe(
+    //   (val) => log('B', val),
+    //   (err) => log('error 2', err),
+    //   () => log('complete 2')
+    // );
+
+    // setTimeout(() => {
+    //   sub.unsubscribe();
+    // }, 1000);
+
+    // setTimeout(() => {
+    //   sub2.unsubscribe();
+    // }, 4000);
+
+    // setTimeout(() => {
+    //   const sub3 = stream$.subscribe(
+    //     (val) => log('C', val),
+    //     (err) => log('error 2', err),
+    //     () => log('complete 2')
+    //   );
+    //   setTimeout(() => {
+    //     sub3.unsubscribe();
+    //   }, 2000);
+    // }, 3000);
+
+    // setTimeout(() => {
+    //   const sub3 = stream$.subscribe(
+    //     (val) => log('D', val),
+    //     (err) => log('error 2', err),
+    //     () => log('complete 2')
+    //   );
+    //   setTimeout(() => {
+    //     sub3.unsubscribe();
+    //   }, 2000);
+    // }, 7000);
+
+
+    // const btn$ = Observable.create((obs) => {
+
+    //   let counter = 0;
+    //   function onClick(e) {
+    //     if (counter > 3) {
+    //       return obs.error('nie nie nie ');
+    //     }
+    //     counter++;
+    //     obs.next(e);
+    //   }
+    //   button.addEventListener('click', onClick);
+
+    //   return () => {
+    //     log('clean your resources');
+    //     button.removeEventListener('click', onClick);
+    //   };
+    // });
+
+    // btn$.subscribe(
+    //   (val) => log('next', val),
+    //   (err) => log('error', err),
+    //   () => log('complete')
+    // );
 
 
   }
--- src/app/rxjs/basic/observable.component.ts
+++ src/app/rxjs/basic/observable.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild, ViewRef, ElementRef } from '@angular/core';
-import { fromEvent, Observable, Observer, Subscription } from 'rxjs';
 import { ListComponent } from 'src/app/shared/list/list.component';
+
+import { fromEvent, Observable, Observer, Subscription } from 'rxjs';
+import { share } from 'rxjs/operators';
 
 @Component({
   selector: 'app-observable',
@@ -23,6 +25,57 @@ export class ObservableComponent implements OnInit {
     const log = (...args) => this.list.add(...args);
     const button = this.btn.nativeElement;
 
+
+    // const btn$: Observable<MouseEvent> = fromEvent(button, 'click');
+
+
+    /**
+     * PRODUCER
+     */
+    const btn$ = Observable.create((obs) => {
+
+      obs.next('custom value 1');
+      // obs.error('custom error');
+      obs.complete();
+      obs.next('custom value 2');
+
+      function onClick(e) {
+        obs.next(e);
+      }
+
+      button.addEventListener('click', onClick);
+
+      return () => {
+        log('koniec pracy');
+        button.removeEventListener('click', onClick);
+      };
+
+    });
+
+    /**
+     * CONSUMER
+     */
+    const observer: Observer<MouseEvent> = {
+      next: (val) => this.list.add('next', val),
+      error: (err) => this.list.add('error', err),
+      complete: () => this.list.add('complete'),
+    };
+
+    const subscription: Subscription = btn$.subscribe(observer);
+    const subscription2: Subscription = btn$.subscribe(observer);
+
+
+    setTimeout(() => {
+      log('unsubscribe');
+      subscription.unsubscribe();
+    }, 2000);
+
+
+    // const sbuscription2 = btn$.subscribe(
+    //   (val) => this.list.add('next', val),
+    //   (err) => this.list.add('error', err),
+    //   () => this.list.add('complete')
+    // );
 
   }
 
@@ -97,5 +150,5 @@ export class ObservableComponent implements OnInit {
       log('timeout unsubscribe');
       subscription.unsubscribe();
     }, 3000);
-    
+
   */
