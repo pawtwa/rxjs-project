68aa6d2b880ad1ca4360d4d7170c486766b0efb4
myflowpl <pedro.blaszczak@gmail.com>
2019-02-23 12:32:40 +0100

    custom filter opeartor

--- src/app/rxjs/basic/pipe.component.ts
+++ src/app/rxjs/basic/pipe.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ListComponent } from 'src/app/shared/list/list.component';
 
 import { Observable, fromEvent, interval, Subject, Subscription } from 'rxjs';
-import { map, distinctUntilChanged, debounceTime, debounce, groupBy, filter, bufferTime, buffer } from 'rxjs/operators';
+import { map, distinctUntilChanged, debounceTime, debounce, groupBy, filter, bufferTime, buffer, retry, switchMap } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
 
 @Component({
   selector: 'app-pipe',
@@ -41,14 +42,24 @@ export class PipeComponent implements OnInit {
     const btn$: Observable<MouseEvent> = fromEvent(button, 'click');
     const input$ = fromEvent<any>(input, 'keyup');
 
+    // const ajax$ = ajax('/api/404-error');
+
+    // ajax$.pipe(
+    //   switchMap(res => ajax$.pipe(retry(2))),
+    //   retry(3)
+    // ).subscribe(
+    //   v => log('V', v),
+    //   err => log('ERR', err)
+    // );
+
     const keyboard$ = input$.pipe(
-      map(e => e.target.value),
+      map<any, string>(e => e.target.value),
       // distinctUntilChanged(),
       myDistinctUntilChanged(),
       // filter(v => v.length > 2),
       myFilter<string>(v => v.length > 2),
       // debounce(() => interval$),
-      // debounceTime(250),
+      debounceTime(250),
       // bufferTime(2000)
       // buffer(interval$)
     );
@@ -80,7 +91,9 @@ function myDistinctUntilChanged() {
 }
 
 function myFilter<T>(compare) {
+
   return function(in$: Observable<T>) {
+
     return Observable.create(obs => {
 
       const sub: Subscription = in$.subscribe(
