398c186dd892cacb1123be9b4956bb1f3387ba7d
myflowpl <pedro.blaszczak@gmail.com>
2019-03-19 19:03:26 +0100

    rename to rxjs

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to rxjs-project!');
+    expect(page.getTitleText()).toEqual('Welcome to rxjs!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'rxjs-project'`, () => {
+  it(`should have as title 'rxjs'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('rxjs-project');
+    expect(app.title).toEqual('rxjs');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to rxjs-project!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to rxjs!');
   });
 });
