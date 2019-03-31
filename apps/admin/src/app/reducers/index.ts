93cf90f9f057444767466ca74b089cfa77faf39a
it-opes <programmer@it-opes.com>
2019-03-31 11:23:38 +0200

    Counter reducer generation

--- apps/admin/src/app/reducers/index.ts
+++ apps/admin/src/app/reducers/index.ts
@@ -6,10 +6,15 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import * as fromCounter from './counter.reducer';
 
-export interface State {}
+export interface State {
+  counter: fromCounter.State;
+}
 
-export const reducers: ActionReducerMap<State> = {};
+export const reducers: ActionReducerMap<State> = {
+  counter: fromCounter.reducer,
+};
 
 export const metaReducers: MetaReducer<State>[] = !environment.production
   ? []
